1a401bc349648ccb827f37655bbfc14d57b5f78c
Dung Nguyen <contact@dungnt.net>
2022-05-14 21:14:13 +0700

    change apollo fake vip method

--- Scripts/apollo.js
+++ Scripts/apollo.js
@@ -1,3 +1,9 @@
-$done({
-    body: JSON.stringify({"is_on": true})
-});
+var resp = JSON.parse($response.body);
+
+resp = {
+  "status_message": "Receipt valid, subscription active, and set to auto-renew.",
+  "subscription_type": "lifetime",
+  "status_code": 0
+}
+
+$done({body: JSON.stringify(resp)});
